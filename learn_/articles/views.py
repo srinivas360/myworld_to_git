06a5e3fe486d5314c0244d01913ba4b93ac950d2
srinivas33333 <srinivasperuri65@gmail.com>
2024-01-03 08:33:05 +0530

    upto article create view function

--- learn_/articles/views.py
+++ learn_/articles/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 from django.contrib.auth.decorators import login_required
+from . import forms
 
 # Create your views here.
 def article_list(req):
@@ -15,4 +16,11 @@ def article_detail(req, slug):
 
 @login_required(login_url=None)
 def article_create(request):
-    return render(request, 'articles/article_create.html')
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid():
+            #save in database
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle()
+    return render(request, 'articles/article_create.html', {'form': form})
