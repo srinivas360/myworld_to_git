91ce64f6e826d754004284f6be465c9181f623ba
srinivas33333 <srinivasperuri65@gmail.com>
2024-01-04 08:32:58 +0530

    artilce list as home page by manuplating url of main app

--- learn_/learn_/urls.py
+++ learn_/learn_/urls.py
@@ -20,11 +20,12 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from articles import views as articles_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.aboutpage),
-    path('', views.homepage),
+    path('', articles_views.article_list, name='home' ),
     path('articles/', include('articles.urls') ),
     path('accounts/', include('accounts.urls')),
 ]
